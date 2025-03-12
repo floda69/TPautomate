65ec484596939e5f6d827ca0a72d8dca049ecb2a
Florian Le Vasseur <152849703+Florian-LeVasseur@users.noreply.github.com>
2025-03-12 16:21:28 +0100

    Update automate.cpp
    commentaires du cpp

--- automate.cpp
+++ automate.cpp
@@ -8,7 +8,7 @@ Automate::Automate(Lexer * l) {
     end = false;
 }
 
-Automate::~Automate() {
+Automate::~Automate() { //on vide les piles de l'automate (i.e., l'état initial et final et l'expression résultat)
     while (pileEtats.size() > 0) {
         delete pileEtats.top();
         pileEtats.pop();
@@ -47,7 +47,7 @@ void Automate::compute(){
         s=lexer->Consulter();
         getCurrentState() -> transition(this,s);
     }
-    if (getCurrentState() ->getID() == 1 && lexer->Consulter()->getId()==FIN)
+    if (getCurrentState() ->getID() == 1 && lexer->Consulter()->getId()==FIN) //on vérifie que l'expression ait été correctement lue
     {
         cout << "Resultat : " << dynamic_cast<Expression*>(getPileSymboles().top())->getResult() << endl;
     }
